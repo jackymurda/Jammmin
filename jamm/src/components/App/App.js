31c9f990631d81598d71c7a616e69b8b915184c9
jackymurda <jamurda@gmail.com>
2020-12-15 15:43:55 +0000

    Jammmin55

---
+++ jamm/src/components/App/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      searchResults: [{name: 'name1', artist: 'artiste1', album:'album1', id:1},{name: 'name2',artist: 'artiste2', album:'album2', id:2},{name: 'name3', artist: 'artiste3', album:'album3', id:3}],playlistName: 'My Playlist', playlistTracks: [{name: 'playlist1', artist: 'artist1', album:'debut', id:4},{name: 'playlist2', artist: 'artist2', album:'great', id:5},{name: 'playlist3', artist: 'artist3', album:'latest', id:6}]
+    };
+     this.addTrack = this.addTrack.bind(this);
+     this.removeTrack = this.removeTrack.bind(this);
+     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+
+  }
+
+  addTrack(track){
+    let plTracks = this.state.playlistTracks;
+    if (plTracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    }
+    plTracks.push(track);
+    this.setState({playlistTracks: plTracks});
+  }
+
+  removeTrack(track) {
+    let pTracks = this.state.playlistTracks;
+    pTracks.filter(currentTrack => currentTrack.id !== track.id );
+    this.setState({playlistTracks: pTracks});
+  }
+
+  updatePlaylistName(name){
+    this.setState({playlistName: name})
+  }
+
+
+  render() {
+    return (
+      <div>
+  <h1>Ja<span className="highlight">mmm</span>ing</h1>
+  <div className="App">
+    <SearchBar />
+    <div className="App-playlist">
+       <SearchResults 
+          searchResults={this.state.searchResults}
+          onAdd={this.addTrack} />
+       <Playlist playlistName={this.state.playlistName}
+                 playlistTracks={this.state.playlistTracks}
+                 onRemove={this.removeTrack}
+                
+      />
+    </div>
+  </div>
+</div>
+    )
+  }
+}
+
+export default App;
